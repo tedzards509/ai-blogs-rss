fd7a445aa38a52870f0cf6ba282e187c28cb17c7
Ted Zards <tjado@edzards.eu>
2026-07-18 16:46:18 +0200

    Use up-to-date agent string

--- feed_generators/util/utils.py
+++ feed_generators/util/utils.py
@@ -22,7 +22,7 @@ from feedgen.feed import FeedGenerator
 # ---------------------------------------------------------------------------
 
 DEFAULT_USER_AGENT = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
+    "Mozilla/5.0 (X11; Linux x86_64; rv:152.0) Gecko/20100101 Firefox/152.0"
 )
 DEFAULT_HEADERS = {"User-Agent": DEFAULT_USER_AGENT}
 
